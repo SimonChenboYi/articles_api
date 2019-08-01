998f74d8f987e7ae0deb075843c67b33799e19f9
Chenbo Yi <yichenbo@outlook.com>
2019-08-01 20:03:43 +0100

    implement create in CRUD

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,12 +5,28 @@ module Api
     class ArticlesController < ApplicationController
       def index
         articles = Article.order('created_at ASC')
-        render json: {status: 'SUCCESS', message:'Loaded articles', data: articles}, status: :ok
+        render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }, status: :ok
       end
 
       def show
         article = Article.find(params[:id])
-        render json: {status: 'SUCCESS', message:'Loaded article', data: article}, status: :ok
+        render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+
+        if article.save
+          render json: { status: 'SUCCESS', message: 'Saved article', data: article }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Article not saved', data: article.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
       end
     end
   end
